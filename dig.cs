94b7ff3a7ada6ae57eb7c38fe3526552cd5fe9ac
Showdowncode <konradbottengranas@gmail.com>
2024-04-10 10:04:32 +0200

    Add ability to place bloks and remove blocks.

---
+++ dig.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class dig : MonoBehaviour
+{
+   public Camera myEyes;
+
+    // Update is called once per frame
+    [System.Obsolete]
+    void Update() {
+        if (Input.GetMouseButtonUp(0)) {
+            AddCube ();
+        }
+
+         if (Input.GetMouseButtonUp(1)) {
+            RemoveCube ();
+        }
+
+    }
+
+
+    void AddCube(){
+
+        Ray ray = myEyes.ScreenPointToRay 
+            (new Vector3(Screen.width/2, Screen.height/2, 0f));
+        RaycastHit hit;
+
+        if (Physics.Raycast (ray, out hit, 5f)) {
+            Vector3 newPos = hit.transform.position;
+            
+            GameObject newB = Instantiate(hit.collider.gameObject);
+
+            newB.transform.position = newPos;
+            newB.transform.Translate(hit.normal * 1f);
+
+
+        }
+
+    }
+
+    [System.Obsolete]
+    void RemoveCube(){
+
+        Ray ray = myEyes.ScreenPointToRay 
+            (new Vector3(Screen.width/2, Screen.height/2, 0f));
+        RaycastHit hit;
+
+        if (Physics.Raycast (ray, out hit, 5f)) {
+            //Vector3 newPos = hit.transform.position;
+            
+            //GameObject newB = Instantiate(hit.collider.gameObject);
+
+            //newB.transform.position = newPos;
+            //newB.transform.Translate(hit.normal * 1f);
+            DestroyObject(hit.collider.gameObject);
+            
+        }
+
+
+    }
+
+}
