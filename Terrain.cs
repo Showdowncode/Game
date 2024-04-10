fe5c4373cc0ab08ee7a6553c9f3e41587a0179fd
Showdowncode <konradbottengranas@gmail.com>
2024-04-10 09:15:27 +0200

    Changed terrain to look better

--- Terrain.cs
+++ Terrain.cs
@@ -6,35 +6,44 @@ using UnityEngine.Audio;
 
 public class Terrain : MonoBehaviour
 {
-    public GameObject currentBlockType;
+    private GameObject currentBlockType;
+    public GameObject[] blockTypes;
 
     public float amp = 10f;
     public float freq = 10f;
 
-    void Start()
-    {
+    private Vector3 myPos;
+
+    void Start() {
         generateTerrain();
     }
 
     void generateTerrain()
+{
+    myPos = this.transform.position;
+    
+    int cols = 100;
+    int rows = 100;
+
+    for (int x = 0; x < cols; x++)
     {
-        int cols = 100;
-        int rows = 100;
-
-        for (int x = 0; x < cols; x++)
+        for (int z = 0; z < rows; z++)
         {
-            for (int z = 0; z < rows; z++)
-            {
-                float y = Mathf.PerlinNoise(x / freq, z / freq) * amp;
+            float y = Mathf.PerlinNoise((myPos.x + x) / freq, (myPos.z + z) / freq) * amp;
 
-                GameObject newBlock = GameObject.Instantiate(currentBlockType);
-                newBlock.transform.position = new Vector3(x, y, z);
+            y = Mathf.Floor (y);
 
-                // Legg til Box Collider til hver blokk
-                BoxCollider collider = newBlock.AddComponent<BoxCollider>();
-                // Juster størrelsen på collidere til å passe til blokken
-                collider.size = new Vector3(1f, 1f, 1f);
-            }
+            if (y > amp/2) currentBlockType =blockTypes[1];
+            else currentBlockType = blockTypes[0];
+
+            GameObject newBlock = GameObject.Instantiate(currentBlockType);
+            newBlock.transform.position = new Vector3(myPos.x + x, y, myPos.z + z);
+
+            // Legg til Box Collider til hver blokk
+            BoxCollider collider = newBlock.AddComponent<BoxCollider>();
+            // Juster størrelsen på collidere til å passe til blokken
+            collider.size = new Vector3(1f, 1f, 1f);
         }
     }
+}
 }
